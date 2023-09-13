883f37577b439a0603af28072c96ba81e3683b7d
Edy4891 <123517645+Edy4891@users.noreply.github.com>
2023-09-12 22:01:07 -0600

    Unidades.java
    checalo jorge, esta medio comentado y la neta esta bien pedorro pero haber que se te hace

--- Unidades.java
+++ Unidades.java
@@ -2,4 +2,36 @@ package Calculadora;
 
 public class Unidades {
    /* 3 métodos */ 
+      public int metrosapulgadas (float valor,int caso ){
+      if (caso==1){
+      float resultado = valor * 39.37f;
+      return resultado;//rsultado en pulgadas
+      }
+      else {
+         float resultado = valor / 39.37f;
+         return resultado;//resultado en metros
+}
+   }
+
+public int metrosayardas (float valor,int caso){//se evalua una variable entera para determinar el caso
+if(caso==1){
+   float resultado = valor * 1.09361f; // Relación de conversión de metros a yardas
+    return resultado;//resultado en yardas
+}
+else {
+   float resultado = valor / 1.09361f; // Relación de conversión de yardas a metros
+    return resultado;//resultado en metros
+}
+}
+
+public int metrosamillas (float valor, int caso){
+   if(caso==1){
+      float resultado = valor * 0.000621371f; // Relación de conversión de metros a millas
+      return resultado;//resultado en millas
+   }
+   else {
+      float resultado = valor / 0.000621371f; // Relación de conversión de millas a metros
+      return resultado;//resultado en metros
+   }
+}
 }

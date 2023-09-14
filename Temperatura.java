84be3f8418a73acf376a9b9baf8289d22ce7878a
Jorge-Alejandro-Lopez-Jimenez <123658274+Jorge-Alejandro-Lopez-Jimenez@users.noreply.github.com>
2023-09-14 14:27:49 -0600

    codigo organizado

--- Temperatura.java
+++ Temperatura.java
@@ -1,41 +1,47 @@
+package Calculadora;
+
 public class Temperatura {
 
     /**
      * Cambia de grados fahrenheit a celsius
+     * 
      * @param Fahrenheit La cantidad en grados fahrenheit a convertir
      * @return La cantidad resultante
      */
-    public static double FahrenheitACelsius(double Fahrenheit){
-        double convertir = (Fahrenheit - 32) * 5.0/9.0;
+    public static double FahrenheitACelsius(double Fahrenheit) {
+        double convertir = (Fahrenheit - 32) * 5.0 / 9.0;
         return convertir;
     }
 
     /**
      * Cambia de grados celsius a fahrenheit
+     * 
      * @param Celsius La cantidad en grados celsius a convertir
      * @return La cantidad resultante
      */
-    public static double CelsiusAFahrenheit(double Celsius){
-        double convertir = (Celsius * 9.0/5.0) + 32;
+    public static double CelsiusAFahrenheit(double Celsius) {
+        double convertir = (Celsius * 9.0 / 5.0) + 32;
         return convertir;
     }
 
     /**
      * Cambia de grados kelvin a celsius
-     * @param  Kelvin La cantidad en grados kelvin a convertir
+     * 
+     * @param Kelvin La cantidad en grados kelvin a convertir
      * @return La cantidad resultante
      */
-    public static double KelvinACelsius(double Kelvin){
+    public static double KelvinACelsius(double Kelvin) {
         double convertir = (Kelvin - 273.15);
         return convertir;
     }
 
     /**
      * Cambia de grados celsius a kelvin
-     * @param Celsius  La cantidad en grados celsius a convertir
+     * 
+     * @param Celsius La cantidad en grados celsius a convertir
      * @return La cantidad resultante
      */
-    public static double CelsiusAKelvin(double Celsius){
+    public static double CelsiusAKelvin(double Celsius) {
         double convertir = (Celsius + 273.15);
         return convertir;
     }
--- Unidades.java
+++ Unidades.java
@@ -2,91 +2,91 @@ package Calculadora;
 
 public class Unidades {
 
-   public float conversor(float entrada, int opcion) {
-       switch (opcion) {
-           case 1:
-               return metrosAPulgadas(entrada);
-           case 2:
-               return pulgadasAMetros(entrada);
-           case 3:
-               return metrosAYardas(entrada);
-           case 4:
-               return yardasAMetros(entrada);
-           case 5:
-               return metrosAMillas(entrada);
-           case 6:
-               return millasAMetros(entrada);
-           case 7:
-               return metrosACentimetros(entrada);
-           case 8:
-               return centimetrosAMetros(entrada);
-           case 9:
-               return metrosAKilometros(entrada);
-           case 10:
-               return kilometrosAMetros(entrada);
-           default:
-               return -1; // Opción no válida
-       }
-   }
+    public float conversor(float entrada, int opcion) {
+        switch (opcion) {
+            case 1:
+                return metrosAPulgadas(entrada);
+            case 2:
+                return pulgadasAMetros(entrada);
+            case 3:
+                return metrosAYardas(entrada);
+            case 4:
+                return yardasAMetros(entrada);
+            case 5:
+                return metrosAMillas(entrada);
+            case 6:
+                return millasAMetros(entrada);
+            case 7:
+                return metrosACentimetros(entrada);
+            case 8:
+                return centimetrosAMetros(entrada);
+            case 9:
+                return metrosAKilometros(entrada);
+            case 10:
+                return kilometrosAMetros(entrada);
+            default:
+                return -1; // Opción no válida
+        }
+    }
 
-   // Método para convertir de metros a pulgadas
-   private float metrosAPulgadas(float valor) {
-       float resultado = valor * 39.37f;
-       return resultado; // Resultado en pulgadas
-   }
+    // Método para convertir de metros a pulgadas
+    private float metrosAPulgadas(float valor) {
+        float resultado = valor * 39.37f;
+        return resultado; // Resultado en pulgadas
+    }
 
-   // Método para convertir de pulgadas a metros
-   private float pulgadasAMetros(float valor) {
-       float resultado = valor / 39.37f;
-       return resultado; // Resultado en metros
-   }
+    // Método para convertir de pulgadas a metros
+    private float pulgadasAMetros(float valor) {
+        float resultado = valor / 39.37f;
+        return resultado; // Resultado en metros
+    }
 
-   //metodo para metros a yardas
-   private float metrosAYardas (float valor) {
-      float resultado = valor * 1.09361f; 
-      return resultado;//resultado en yardas
-   }
+    // metodo para metros a yardas
+    private float metrosAYardas(float valor) {
+        float resultado = valor * 1.09361f;
+        return resultado;// resultado en yardas
+    }
 
-   //metodo para yardas a metros
-   private float yardasAMetros (float valor) {
-      float resultado = valor / 1.09361f; 
-    return resultado;//resultado en metros
-   }
+    // metodo para yardas a metros
+    private float yardasAMetros(float valor) {
+        float resultado = valor / 1.09361f;
+        return resultado;// resultado en metros
+    }
 
-   //metodo para metros a millas
-   private float metrosAMillas (float valor) {
-      float resultado = valor * 0.000621371f;
-      return resultado;//resultado en millas
-   }
+    // metodo para metros a millas
+    private float metrosAMillas(float valor) {
+        float resultado = valor * 0.000621371f;
+        return resultado;// resultado en millas
+    }
 
-   //metodo para millas a metros
-   private float millasAMetros (float valor) {
-      float resultado = valor / 0.000621371f;
-      return resultado;//resultado en metros
-   }
+    // metodo para millas a metros
+    private float millasAMetros(float valor) {
+        float resultado = valor / 0.000621371f;
+        return resultado;// resultado en metros
+    }
 
-   //metodo para metros a centimetros
-   private float metrosACentimetros (float valor) {
-      float resultado = valor * 100f;
-      return resultado;//resultado en cm
-   }
+    // metodo para metros a centimetros
+    private float metrosACentimetros(float valor) {
+        float resultado = valor * 100f;
+        return resultado;// resultado en cm
+    }
 
-   //metodo para centimetros a metros
-   private float centimetrosAMetros (float valor) {
-      float resultado = valor / 100f;
-      return resultado;//resultado en metros
-   }
+    // metodo para centimetros a metros
+    private float centimetrosAMetros(float valor) {
+        float resultado = valor / 100f;
+        return resultado;// resultado en metros
+    }
 
-   //metodo para metros a kilometros
-   private float metrosAKilometros (float valor) {
-      float resultado = valor / 1000f;
-      return resultado;//resultado en kilometros
-   }
+    // metodo para metros a kilometros
+    private float metrosAKilometros(float valor) {
+        float resultado = valor / 1000f;
+        return resultado;// resultado en kilometros
+    }
 
-   //metodo para kilometros a metros
-   private float kilometrosAMetros (float valor) {
-      float resultado = valor * 1000f;
-      return resultado;//resultado en metros
-   }
-   
+    // metodo para kilometros a metros
+    private float kilometrosAMetros(float valor) {
+        float resultado = valor * 1000f;
+        return resultado;// resultado en metros
+    }
+
 }

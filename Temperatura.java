4cc08a0b3b12a63a1ed7dd7dbfbf70b03737ebeb
NoradeAnda <123666852+NoradeAnda@users.noreply.github.com>
2023-09-14 01:44:00 -0600

    Update Temperatura.java

--- Temperatura.java
+++ Temperatura.java
@@ -2,4 +2,23 @@ package Calculadora;
 
 public class Temperatura {
     
+    public static double FahrenheitACelsius(double Fahrenheit){
+        double convertir = (Fahrenheit - 32) * 5.0/9.0;
+        return convertir;
+    }
+
+    public static double CelsiusAFahrenheit(double Celsius){
+        double convertir = (Celsius * 9.0/5.0) + 32;
+        return convertir;
+    }
+
+    public static double KelvinACelsius(double Kelvin){
+        double convertir = (Kelvin - 273.15);
+        return convertir;
+    }
+
+    public static double CelsiusAKelvin(double Celsius){
+        double convertir = (Celsius + 273.15);
+        return convertir;
+    }
 }
